bf3a5af99da0c721ef939917530e35b87d506333
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-05-04 21:39:08 -0300

    refactor: melhora nomes de funções e organização do gerador CPF

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,94 +1,101 @@
-function main() {
+function gerarCPF() {
     const container = document.getElementById("resposta");
     const opcaoSelecionada = document.querySelector('input[name="format"]:checked');
 
-
     container.innerHTML = '';
 
-    let cpf = ''; 
-    let tamanho;
+    let cpf = '';
+    let numerosCPF;
+
     do {
-        tamanho = geraNumerosBase(0, 9);
-    } while (pegaSequenciaRepetida(tamanho));
-  
-    let digitoValidador1 = gerarDigitosValidadores(tamanho);
-    tamanho.push(digitoValidador1);
-    let digitoValidador2 = gerarDigitosValidadores(tamanho);
-    tamanho.push(digitoValidador2);
+        numerosCPF = gerarNumerosAleatorios(0, 9);
+    } while (cpfSequenciaRepetida(numerosCPF));
+
+    let digitoValidador1 = gerarDigitoValidador(numerosCPF);
+    numerosCPF.push(digitoValidador1);
+
+    let digitoValidador2 = gerarDigitoValidador(numerosCPF);
+    numerosCPF.push(digitoValidador2);
 
     if (opcaoSelecionada.value === "true") {
-       cpf = gerarPotucaoCPF(tamanho); 
-    }else{
-        cpf = limparCPF(tamanho);
+        cpf = formatarCPF(numerosCPF);
+    } else {
+        cpf = juntarNumerosCPF(numerosCPF);
     }
-   container.innerHTML = cpf;
+
+    container.innerHTML = cpf;
 }
 
-
-function geraNumerosBase(min, max) {
+function gerarNumerosAleatorios(min, max) {
     let numerosBase = [];
-    for(let i = 0; i < 9; i++)
-    {
-        const numeroAleatorio =  Math.floor(Math.random() * (max - min + 1)) + min; 
+
+    for (let i = 0; i < 9; i++) {
+        const numeroAleatorio =
+            Math.floor(Math.random() * (max - min + 1)) + min;
+
         numerosBase.push(numeroAleatorio);
     }
+
     return numerosBase;
 }
 
-function gerarDigitosValidadores(numerosBase){
-
-    let contador = numerosBase.length  + 1;
-    let somador  = 0;
+function gerarDigitoValidador(numerosBase) {
+    let contador = numerosBase.length + 1;
+    let somador = 0;
     let resto = 0;
 
     numerosBase.forEach(numero => {
         somador += numero * contador;
         contador--;
     });
-    
+
     resto = calcularResto(somador);
+
     return resto;
 }
 
-function calcularResto(somaTotal){
+function calcularResto(somaTotal) {
+    let restoDivisao = (somaTotal * 10) % 11;
 
-    let restoDIvisao = 0;
-
-    restoDIvisao = (somaTotal * 10) % 11;
-
-    if(restoDIvisao == 10){
-        restoDIvisao = 0;
+    if (restoDivisao === 10) {
+        restoDivisao = 0;
     }
-    return restoDIvisao;      
+
+    return restoDivisao;
 }
 
-function copiarCPF(){
+function copiarCpfGerado() {
     const buscaCPF = document.getElementById("resposta");
-    const innerHtmlCPF = buscaCPF.innerHTML
+    const cpfTexto = buscaCPF.innerHTML;
 
-    navigator.clipboard.writeText(innerHtmlCPF).then(() => {
-        alert('Copiado com Sucesso !!')
-    }).catch(err => {
-        console.error("Erro ao copiar: ", err);
-    })
+    navigator.clipboard.writeText(cpfTexto)
+        .then(() => {
+            alert("Copiado com sucesso!");
+        })
+        .catch(err => {
+            console.error("Erro ao copiar:", err);
+        });
 }
 
-function gerarPotucaoCPF(valor){
-    const cpfTexto = limparCPF(valor);
-    const cpfFormatado =  cpfTexto.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-    return cpfFormatado;
+function formatarCPF(valor) {
+    const cpfTexto = juntarNumerosCPF(valor);
+
+    return cpfTexto.replace(
+        /(\d{3})(\d{3})(\d{3})(\d{2})/,
+        "$1.$2.$3-$4"
+    );
 }
 
-function limparCPF(valor){
-    const cpfTexto = valor.join("");
-    return cpfTexto;
+function juntarNumerosCPF(valor) {
+    return valor.join("");
 }
 
-function pegaSequenciaRepetida(vetorCPF) {
+function cpfSequenciaRepetida(vetorCPF) {
     for (let i = 1; i < vetorCPF.length; i++) {
         if (vetorCPF[i] !== vetorCPF[0]) {
-            return false; 
+            return false;
         }
     }
-    return true; 
+
+    return true;
 }
